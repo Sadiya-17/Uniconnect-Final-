497c398ccf239d73377c249721abb910b865a5c7
Sadiya-17 <sadiahossain680@gmail.com>
2026-01-03 11:39:15 +0600

    MongoDB connection and User model added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,14 @@
-import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 5000;
+import app from "./app";
+import { connectDB } from "./config/db";
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
